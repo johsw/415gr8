16964123aa3b71cfee5f472f29913e773e39df17
Johannes Wehner <johsw@information.dk>
2012-10-27 23:40:56 +0200

    Stole some stuff from mobile boilerplate...

--- client/js/main.js
+++ client/js/main.js
@@ -3,8 +3,7 @@ socket = io.connect('http://localhost', {port:8080});
 $(document).ready(function() {
   socket.on('connect', function () {
     socket.on('update', function (count) {
-      $('#counter').hide();
-      $('#counter').text(count).fadeIn("slow");
+      $('#counter').hide().text(count).fadeIn("slow");
     });
   });
   $('#button').click(function() {
